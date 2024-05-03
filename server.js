b03d13e3f66d7305d8de7c412e18ab15ff8a0c5c
carrot004 <aoduythinh@gmail.com>
2024-05-03 13:09:50 +0700

    aa
    bb

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+const dbConfig = require('./db')
+
 const port  = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Node server started using nodemon`)); 
