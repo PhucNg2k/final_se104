2cf0e894c2e22de78463eb09a64222f2937c7048
PhucTH <22521134@gm.uit.edu.vn>
2024-05-03 15:28:49 +0700

    update proxy

--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -3,15 +3,21 @@ import axios from 'axios'
 
 
 function Homescreen() {
-
+    
     const [rooms, setRooms] = useState([])
 
     useEffect(() => {
+
         const fetchData = async () => {
             try {
+                console.log('123')
                 const data = (await axios.get('/api/rooms/getallrooms')).data;
-                setRooms(data);
+                console.log('data', data);
+                
+                setRooms(data)
+                console.log(rooms)
             } catch (error) {
+                
                 console.log(error);
             }
         };
@@ -22,8 +28,8 @@ function Homescreen() {
 
     return (
         <div>
-            <h1>Home screen</h1>
-            <h1>there are {rooms.length} rooms</h1>
+            <h1>Home screen 1</h1>
+            <h1>there are 5 {rooms.length} rooms</h1>
         </div>
     )
 }
