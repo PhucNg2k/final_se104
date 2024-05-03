0902d431c5a16f38a872258b6ded8fefa40b72f9
PhucTH <22521134@gm.uit.edu.vn>
2024-05-03 15:04:46 +0700

    Get all rooms

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import Homescreen from './screens/Homescreen';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'; 
+
 function App() {
   return (
     <div className='App'>
       <Navbar/>
+
+      
+      <BrowserRouter>
+      <Routes>
+        <Route path='/home' element={<Homescreen />} />
+      </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
--- server.js
+++ server.js
@@ -7,6 +7,6 @@ const roomsRoute = require("./routes/roomsRoute")
 
 app.use("/api/rooms",roomsRoute)
 
-const port  = process.env.PORT || 3000;
+const port  = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Node server started using nodemon`)); 
