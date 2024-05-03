50b3249bc40b061bad10b5522f9a274c4e22d04a
PhucTH <22521134@gm.uit.edu.vn>
2024-05-03 15:08:58 +0700

    Update Homecreen.js

--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -4,13 +4,26 @@ import axios from 'axios'
 
 function Homescreen() {
 
-    
+    const [rooms, setRooms] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const data = (await axios.get('/api/rooms/getallrooms')).data;
+                setRooms(data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        fetchData(); // Call the async function immediately
+    }, []);
 
 
     return (
         <div>
             <h1>Home screen</h1>
-            
+            <h1>there are {rooms.length} rooms</h1>
         </div>
     )
 }
