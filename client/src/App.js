b26de49262338961925272e94f5d29414cdad5f6
ostic71 <thanhtienle71@gmail.com>
2024-05-12 01:33:02 +0700

    15
    12/5

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import Navbar from './components/Navbar';
 import Homescreen from './screens/Homescreen';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'; 
 import Bookingscreen from './screens/Bookingscreen';
+import Registerscreen from './screens/Registerscreen';
+import Loginscreen from './screens/Loginscreen';
+
 
 function App() {
   return (
@@ -16,8 +19,8 @@ function App() {
       <Routes>
         <Route path='/home' element={<Homescreen />} />
         <Route path='/book/:roomid' exact Component={Bookingscreen} />
-      
-      
+        <Route path='/register' exact Component={Registerscreen}/>
+        <Route path='/login' exact Component={Loginscreen}/>
       </Routes>
       
       
